2e9f6d6e6619cad9ec1465c95eba8acb57b4e709
Dolma <dolma1918@gmail.com>
2024-03-19 21:26:42 +0545

    feat: Class component created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
+import Welcome from './components/Welcomee'
+import React, {Component} from 'react'
 
-function App() {
+class App extends Component {
+  render () {
   return (
     <div className="App">
       <Greet />
+      <Welcome />
      
     </div>
-  );
+  )};
 }
 
 export default App;
